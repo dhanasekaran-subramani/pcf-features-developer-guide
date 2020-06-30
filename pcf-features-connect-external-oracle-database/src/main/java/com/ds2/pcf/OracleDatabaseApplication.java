acc5f9469404d5f655348138053bb89141616a1c
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-06-30 19:10:58 +0530

    initial spring boot project structure

---
+++ pcf-features-connect-external-oracle-database/src/main/java/com/ds2/pcf/OracleDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.ds2.pcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleDatabaseApplication.class, args);
+	}
+
+}
