755256fb3959f72ad61253886634a9fc30737c67
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-07-01 19:52:29 +0530

    Spring boot initial project setup

---
+++ pcf-features-scheduler/src/test/java/com/ds2/pcf/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds2.pcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
